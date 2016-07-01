504bc689f380fc21a235fa95797a38d99a14adeb
Eric Fine <wsuerk@gmail.com>
2016-07-01 18:00:11 -0400

    initial commit

---
+++ Assets/Scripts/ParentTranlation.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class ParentTranlation : MonoBehaviour {
+    SteamVR_TrackedObject trackedObj;
+    SteamVR_Controller.Device device;
+
+    public Transform sphere;
+    void Awake() {
+        Debug.Log("GAME AWAKE");
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	void FixedUpdate () {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Trigger Touch");
+        }
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Trigger Touch Down");
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Trigger Touch Up");
+        }
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Trigger Press");
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Trigger Press Down");
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Trigger Press Up");
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            resetSphere();
+        }
+    }
+
+    private void resetSphere()
+    {
+        Rigidbody sphereBody = sphere.GetComponent<Rigidbody>();
+        sphereBody.velocity = Vector3.zero;
+        sphereBody.angularVelocity = Vector3.zero;
+        sphere.transform.position = Vector3.zero;
+
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        Debug.Log("You have collided with " + col.name);
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have Grabbed " + col.name);
+            col.attachedRigidbody.isKinematic = true;
+            col.gameObject.transform.SetParent(this.gameObject.transform);
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have let go of " + col.name);
+            col.gameObject.transform.SetParent(null);
+            col.attachedRigidbody.isKinematic = false;
+            objectTossed(col.GetComponent<Rigidbody>());
+        }
+    }
+
+    private void objectTossed(Rigidbody rigidbody)
+    {
+        Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+        rigidbody.velocity = origin.TransformVector(device.velocity);
+        rigidbody.angularVelocity = origin.TransformVector(device.angularVelocity);
+    }
+}
---
+++ Assets/Scripts/PointTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PointTrigger : MonoBehaviour {
+    private GameObject scoreObject;
+    private int score = 0;
+    // Use this for initialization
+    void Awake()
+    {
+        scoreObject = GameObject.Find("New Text");
+        TextMesh thing = scoreObject.GetComponent<TextMesh>();
+        thing.text = getScore();
+    }
+
+    private string getScore()
+    {
+        return "Score: " + score;
+    }
+
+    void OnTriggerEnter(Collider ball)
+    {
+        score++;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        scoreObject = GameObject.Find("New Text");
+        TextMesh thing = scoreObject.GetComponent<TextMesh>();
+        thing.text = getScore();
+    }
+}
